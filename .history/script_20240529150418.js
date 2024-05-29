c2d8687740dfb2813bdb519d1ce653c1dd4e50ff
pyrcl090 <70988666+pyrcl090@users.noreply.github.com>
2024-05-29 15:04:33 +0200

    update

---
+++ .history/script_20240529150418.js
@@ -0,0 +1,86 @@
+let img1 = document.querySelector(".img1");
+let img2 = document.querySelector(".img2");
+// let img3 = document.querySelector(".img3");
+let img4 = document.querySelector(".img4");
+// let img5 = document.querySelector(".img5");
+let img6 = document.querySelector(".img6");
+let img7 = document.querySelector(".img7");
+÷
+let img9 = document.querySelector(".img9");
+let img10 = document.querySelector(".img10");
+// let img11 = document.querySelector(".img11");
+// let img12 = document.querySelector(".img12");
+
+let imgtxt = document.querySelector("#img_text");
+// let mp4txt = document.querySelector("#mp4_text");
+
+img1.addEventListener("mouseover", function(){
+    // mp4txt.innerHTML = " ";
+    imgtxt.innerHTML = "labraderie_POSTER";
+});
+
+img2.addEventListener("mouseover", function(){
+    // mp4txt.innerHTML = " ";
+    imgtxt.innerHTML = "rusałka_TYPEFACE";
+});
+
+// img3.addEventListener("mouseover", function(){
+//     mp4txt.innerHTML = "";
+//     imgtxt.innerHTML = "perfectdesigns_POSTER";
+// });
+
+img4.addEventListener("mouseover", function(){
+    // mp4txt.innerHTML = " ";
+    imgtxt.innerHTML = "sledz_BRANDING";
+});
+
+// img5.addEventListener("mouseover", function(){
+//     // mp4txt.innerHTML = " ";
+//     imgtxt.innerHTML = "yasashii_EXHIBITION";
+// });
+
+img6.addEventListener("mouseover", function(){
+    // mp4txt.innerHTML = " ";
+    imgtxt.innerHTML = "yasashii_EXHIBITION";
+});
+
+img7.addEventListener("mouseover", function(){
+    // mp4txt.innerHTML = " ";
+    imgtxt.innerHTML = "(e)3_BOOK";
+});
+
+img8.addEventListener("mouseover", function(){
+    // mp4txt.innerHTML = " ";
+    imgtxt.innerHTML = "nadine_EXHIBITION";
+});
+
+img9.addEventListener("mouseover", function(){
+    // mp4txt.innerHTML = " ";
+    imgtxt.innerHTML = "recettesgenerees_BOOK";
+});
+
+img10.addEventListener("mouseover", function(){
+    // mp4txt.innerHTML = " ";
+    imgtxt.innerHTML = "perishables_BOOK";
+});
+
+// img11.addEventListener("mouseover", function(){
+//     // mp4txt.innerHTML = " ";
+//     imgtxt.innerHTML = "(e)2_BOOK";
+// });
+
+// img12.addEventListener("mouseover", function(){
+//     // mp4txt.innerHTML = " ";
+//     imgtxt.innerHTML = "(e)2_BOOK";
+// });
+
+
+// event listener text
+// labraderie
+// whitefinproject
+// sledz
+// preciouscarharttWIP
+// signaletiquechiottes
+// (e)
+// lepasdecote
+// recettesgenerees
